5ec4c356a3f8253a5fc846fb85d83bac8bc3b45a
sebaspa <sebaspa0107@gmail.com>
2025-11-28 12:28:59 -0500

    Carga y muestra de imagen

--- app/(routes)/teacher/[courseId]/components/CourseImage/CourseImage.tsx
+++ app/(routes)/teacher/[courseId]/components/CourseImage/CourseImage.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import axios from 'axios'
 
 import { FileImage, Pencil } from 'lucide-react'
 
@@ -8,19 +9,53 @@ import TitleBlock from '../TitleBlock/TitleBlock'
 import { CourseImageProps } from './CourseImage.types'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import { UploadButton } from '@/utils/uploadthing'
+import { toast } from 'sonner'
 
 const CourseImage = (props: CourseImageProps) => {
   const { idCourse, imageCourse } = props
   const [isEditing, setIsEditing] = useState(false)
+  const [image, setImage] = useState(imageCourse)
+
+  const onChangeImage = async (imageUrl: string) => {
+    console.log(imageUrl)
+
+    try {
+      axios.patch(`/api/course/${idCourse}`, {
+        imageUrl
+      })
+      toast.success('Imagen actualizada')
+    } catch (error) {
+      console.error(error)
+      toast.error('Error al actualizar la imagen')
+    }
+  }
+
   return (
     <div className='p-4 rounded-lg bg-white h-fit'>
       <TitleBlock title='Imagen del curso' icon={FileImage} />
       {isEditing ? (
-        <p>Upload image</p>
+        <div className='bg-slate-300 p-4 mt-2 rounded-lg'>
+          <UploadButton
+            endpoint='imageUploader'
+            onClientUploadComplete={res => {
+              onChangeImage(res[0]?.ufsUrl || '')
+              setImage(res[0]?.ufsUrl)
+              setIsEditing(false)
+            }}
+            onUploadError={error => {
+              toast.error('Error al subir la imagen')
+              console.log('Error: ', error)
+            }}
+            onUploadProgress={progress => {
+              console.log('Progress: ', progress)
+            }}
+          />
+        </div>
       ) : (
-        <div className='w-full h-full min-h-[250px] bg-gray-300 rounded-md'>
+        <div className='w-full h-full min-h-5 bg-gray-300 rounded-md'>
           <Image
-            src={imageCourse || '/default-course-image.webp'}
+            src={image || '/default-course-image.webp'}
             alt='Imagen del curso'
             width={500}
             height={250}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ayf880m0rc.ufs.sh"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
